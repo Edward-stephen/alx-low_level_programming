705535477d3f1479cad08321098fca7a5a006d67
Edward-Stephen <edwardstephen1998@gmail.com>
2022-09-08 12:19:36 -0700

    ‘puts’

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 /**
  *
- *  * main - Entry point
+ *   main - Entry point
  *
  *   Description: using the main function
  *   this program prints "Programming is like building a multilingual puzzle
